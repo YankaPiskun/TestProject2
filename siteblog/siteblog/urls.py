79e33e413c71f8099e9b09a59b78e6031504ea44
YankaPiskun <yan.piskun.2000@bk.ru>
2022-01-23 00:04:23 +0300

    register models in admin and added debug_toolbar, ckeditor

--- siteblog/siteblog/urls.py
+++ siteblog/siteblog/urls.py
@@ -21,7 +21,11 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
 
 if settings.DEBUG:
+    urlpatterns = [
+        path('__debug__/', include('debug_toolbar.urls')),
+    ]+ urlpatterns
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
